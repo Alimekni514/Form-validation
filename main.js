d4c8f8a042bbdab0b2f0f5aa39623222e9497b85
Alimekni514 <86629769+Alimekni514@users.noreply.github.com>
2022-02-21 22:12:22 +0100

    Upadte js

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ form.addEventListener("submit",(e)=> {
 
 });
 function checkInputs () {
+    let x=0;
     let firstnameValue=firstname.value.trim();
     let lastnameValue=lastname.value.trim();
     let passwordValue=password.value.trim();
@@ -20,6 +21,7 @@ function checkInputs () {
     let emailValue=email.value.trim();
     let phonenumberValue=phonenumber.value.trim();
     let dobValue=dob.value;
+    let tableau=document.getElementById("tableau");
 
     // mata3 check alphabetic user
     let chiffre =checkuser(firstname);
@@ -32,6 +34,7 @@ function checkInputs () {
         setErrorFor(firstname,"First Name must be alphabetic");
     }else {
         setSuccessFor(firstname);
+         x++;
     }
     //check Last Name
     if(lastnameValue==="") {
@@ -40,6 +43,7 @@ function checkInputs () {
         setErrorFor(lastname,"last Name must be alphabetic");
     }else {
         setSuccessFor(lastname);
+        x++
     }
     //check email
     if(emailValue === '') {
@@ -48,6 +52,7 @@ function checkInputs () {
 		setErrorFor(email, "Not a valid email");
 	} else {
 		setSuccessFor(email);
+       x++
 	}
     //check phone Number
     if(phonenumberValue==="") {
@@ -56,6 +61,7 @@ function checkInputs () {
         setErrorFor(phonenumber, "phone number must only be 8 numbers");
     }else {
         setSuccessFor(phonenumber);
+        x++
     }
   
     //check password
@@ -65,6 +71,7 @@ function checkInputs () {
         setErrorFor(password, "Not a valid password");
     }else {
         setSuccessFor(password);
+        x++
     }
     //check password 2
     if(password2Value==="") {
@@ -73,6 +80,7 @@ function checkInputs () {
             setErrorFor(password2,"Password does not match");
     }else {
         setSuccessFor(password2);
+        x++
     }
     //check date of birth
     if(dobValue==="") {
@@ -81,7 +89,47 @@ function checkInputs () {
         setErrorFor(dob,"You must be 18");
     }else {
         setSuccessFor(dob);
+        x++
     }
+    //check all 
+        if(x==7) {
+            tableau.style.cssText="visibility:visible";
+            remplirtableau(firstname,lastname,password,phonenumber,email,dob);
+            
+        }
+    let childElements = document.getElementsByClassName('todelete');
+    let  buttonElement = document.getElementsByClassName('delete');
+    let  buttonElement2 = document.getElementsByClassName('update');
+//To delete a user
+let _handler = function(e) {
+     e.target.parentNode.parentNode.remove();
+}
+function deleteItem(buttonsClass, childClass) {
+  for (var i=0;i<buttonsClass.length;i++) {
+    buttonsClass[i].addEventListener('click', _handler, false);
+  }    
+}
+deleteItem(buttonElement, childElements);
+//to update a user 
+let _update=function(e) {
+    let x=e.target.parentNode.parentNode;
+    let y= x.children;
+    console.log(y);
+    firstname.value=y[0].textContent;
+    lastname.value=y[1].textContent;
+    email.value=y[3].textContent;
+    phonenumber.value=y[2].textContent;
+    password.value=y[4].textContent;
+    password2.value=y[4].textContent;
+    dob.value=y[5].textContent;
+}
+function UpdateItem(buttonsClass, childClass) {
+
+    for (var i=0;i<buttonsClass.length;i++) {
+      buttonsClass[i].addEventListener('click', _update, false);
+    }    
+  }
+  UpdateItem(buttonElement2, childElements);
 
 }
 function setErrorFor(input,message) {
@@ -121,4 +169,52 @@ function dateofbirth (birth) {
     }
 }
 console.log(dateofbirth(dobValue));
+function remplirtableau(first,last,pass,phone,mail,birth) {
+    first=firstname.value;
+    last=lastname.value;
+    pass=password.value;
+    phone=phonenumber.value;
+    mail=email.value;
+    birth=dob.value;
+    let tbody=document.getElementById("tabbody");
+    let trow=document.createElement("tr");
+    let td1=document.createElement("td");
+    let td2=document.createElement("td");
+    let td3=document.createElement("td");
+    let td4=document.createElement("td");
+    let td5=document.createElement("td");
+    let td6=document.createElement("td");
+    let td7=document.createElement("td");
+    let btn1=document.createElement("button");
+    let btn2=document.createElement("button");
+    btn1.innerHTML="Delete";
+    btn2.innerHTML="Update";
+    btn1.setAttribute("class","delete");
+    btn2.setAttribute("class","update");
+    btn1.style.cssText="display:block;color:white;background-color:red;padding:9px;border:none;margin-bottom:5px";
+    btn2.style.cssText="display:block;color:white;background-color:cyan;padding:8px;border:none";
 
+    console.log(btn1);
+    //nabdew bl td 
+    td1.innerHTML=first;
+    console.log(td1);
+    td2.innerHTML=last;
+    td3.innerHTML=phone;
+    td4.innerHTML=mail;
+    td5.innerHTML=pass;
+    td6.innerHTML=birth;
+    td7.appendChild(btn1);
+    td7.appendChild(btn2);
+    //tawa nhotou data fi west row
+    trow.appendChild(td1);
+    trow.appendChild(td2);
+    trow.appendChild(td3);
+    trow.appendChild(td4);
+    trow.appendChild(td5);
+    trow.appendChild(td6);
+    trow.appendChild(td7);
+    trow.setAttribute("class","todelete");
+    console.log(trow);
+    //taw nhotou row fi west table
+    tbody.appendChild(trow);
+}
